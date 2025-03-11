80b4163d4207785866511dd5451de7e8fc8e939f
Huzaim-cmd <huzaimkhan96@gmail.com>
2025-03-11 13:19:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+
+# Download necessary NLTK resources
+try:
+    nltk.data.find('tokenizers/punkt')
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    
+    # Use a simpler tokenization approach
+    text = text.split()
+    
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+try:
+    tfidf = pickle.load(open('vectorizer.pkl','rb'))
+    model = pickle.load(open('model.pkl','rb'))
+except FileNotFoundError:
+    st.error("Model files not found. Please make sure 'vectorizer.pkl' and 'model.pkl' are in the current directory.")
+    st.stop()
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+    if input_sms:
+        # 1. preprocess
+        transformed_sms = transform_text(input_sms)
+        # 2. vectorize
+        vector_input = tfidf.transform([transformed_sms])
+        # 3. predict
+        result = model.predict(vector_input)[0]
+        # 4. Display
+        if result == 1:
+            st.header("Spam")
+        else:
+            st.header("Not Spam")
+    else:
+        st.warning("Please enter a message to classify.")
